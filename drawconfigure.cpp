042a4896e4e96672ba2d371bd0179075a1e54e0a
IDlab <idlab.saba@gmail.com>
2015-12-22 10:32:37 +0900

    [mod]: writing json io

--- drawconfigure.cpp
+++ drawconfigure.cpp
@@ -1,4 +1,27 @@
 #include "drawconfigure.h"
+#include <QFile>
+#include <QTextStream>
+#include <QJsonDocument>
+#include <QJsonObject>
+
+DrawConfigure* DrawConfigure::fromJsonFile(const QString &filename,QObject* parent){
+    QFile f(filename);
+    if(f.open(QFile::ReadOnly)){
+        QTextStream stream(&f);
+        return DrawConfigure::fromJsonString(stream.readAll(), parent);
+    }
+    return DrawConfigure::defaultConfigure(parent);
+}
+
+DrawConfigure* DrawConfigure::fromJsonString(const QString &text,QObject* parent){
+    QJsonDocument doc = QJsonDocument::fromJson(text);
+    QJsonObject obj = doc.object();
+    return new DrawConfigure(parent);
+}
+
+DrawConfigure* DrawConfigure::defaultConfigure(QObject *parent){
+    return new DrawConfigure(parent);
+}
 
 DrawConfigure::DrawConfigure(QObject *parent) : QObject(parent)
 {
--- drawconfigure.h
+++ drawconfigure.h
@@ -2,15 +2,25 @@
 #define DRAWCONFIGURE_H
 #include <QObject>
 
+#include <QList>
+#include <QPair>
+#include <QString>
+
 class DrawConfigure : public QObject
 {
     Q_OBJECT
 public:
+    static DrawConfigure* fromJsonFile(const QString& filename, QObject* parent = 0);
+    static DrawConfigure* fromJsonString(const QString& text, QObject* parent = 0);
+    static DrawConfigure* defaultConfigure(QObject* parent = 0);
     explicit DrawConfigure(QObject *parent = 0);
+    const QList<QPair<QString, QString>> getConnectJoints;
 
 signals:
 
 public slots:
+private:
+    QList<QPair<QString, QString>> connect_joints;
 };
 
 #endif // DRAWCONFIGURE_H
