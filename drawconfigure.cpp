c6a82524be772c23af0ebbd5fc88327d8476d239
IDlab <idlab.saba@gmail.com>
2015-12-22 09:18:42 +0900

    [add]: add draw configure class

---
+++ drawconfigure.cpp
@@ -0,0 +1,6 @@
+#include "drawconfigure.h"
+
+DrawConfigure::DrawConfigure(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ drawconfigure.h
@@ -0,0 +1,16 @@
+#ifndef DRAWCONFIGURE_H
+#define DRAWCONFIGURE_H
+
+
+class DrawConfigure : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DrawConfigure(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // DRAWCONFIGURE_H
