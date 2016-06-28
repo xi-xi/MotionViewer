fc3a74aa23eda6059b873671c35927782613c8f4
IDlab <idlab.saba@gmail.com>
2016-06-28 17:47:40 +0900

    [add]: add doc for motion engine

--- motiongeometryengine.h
+++ motiongeometryengine.h
@@ -10,12 +10,43 @@ class QOpenGLTexture;
 class BoxGeometryEngine;
 class DrawConfigure;
 
+/*!
+ * \brief モーションを描画する諸々を保持するクラス
+ *
+ * モーションを描画するのに必要となる，
+ *
+ * \li BoxGeometryEngine
+ * \li DrawConfigure
+ * \li テクスチャ
+ *
+ * を内部的に保持しています．
+ * また，それらを活用することにより，ポーズを描画することが可能です．
+ *
+ * ポーズの描画のために，このクラスは多少複雑な処理を行っていますので，内容について詳細に書いておきます．
+ * まず，このクラスはDrawConfigureを参照します．
+ * それにより，ポーズの描画にあたって必要となる，関節の接続情報を取得します．
+ * 次に，関節の接続の数(ボーンの数)だけBoxGeometryEngineを作成します．
+ * 最後に，各ボーンに対応するように長さや位置を設定してBoxを描画することで，スティックピクチャを描画します．
+ */
 class MotionGeometryEngine
 {
 public:
+    /*!
+     * \brief コンストラクタ
+     */
     MotionGeometryEngine();
+
+    /*!
+     * \brief デストラクタ
+     */
     virtual ~MotionGeometryEngine();
 
+    /*!
+     * \brief 必要な情報をもとに，Poseを描画します
+     * \param program シェーダプログラム
+     * \param vp_matrix view行列とprojection行列をかけたもの
+     * \param pose 描画するPose
+     */
     void drawMotionGeometry(QOpenGLShaderProgram* program, const QMatrix4x4& vp_matrix, const Pose* pose);
 
 private:
